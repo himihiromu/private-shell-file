0b06113d4f7065bca9b58454511c1a34e17fcaaf
himihiromu <himihiromu@icloud.com>
2024-12-15 07:37:42 +0900

    fix: バックスラッシュはだめみたい

--- pwsh-scoop-setting.ps1
+++ pwsh-scoop-setting.ps1
@@ -1,6 +1,3 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
-scoop install 7zip adb aomei-partition-assistant-standard appium bun cmake cpu-z crystaldiskinfo dark deepl discord \
-  eclipse-java epic-games-launcher filezilla firefox gimp git go googlechrome gradle innounp jetbrains-toolbox latex \
-  libreoffice musicbee nmap nvm openjdk openjdk22 openssh openssl pandoc pipx ppsspp putty pwsh python rufus rustup \
-  skype slack teamviewer teraterm vivaldi vlc vscode winscp xampp zoom
+scoop install 7zip adb aomei-partition-assistant-standard appium bun cmake cpu-z crystaldiskinfo dark deepl discord eclipse-java epic-games-launcher filezilla firefox gimp git go googlechrome gradle innounp jetbrains-toolbox latex libreoffice musicbee nmap nvm openjdk openjdk22 openssh openssl pandoc pipx ppsspp putty pwsh python rufus rustup skype slack teamviewer teraterm vivaldi vlc vscode winscp xampp zoom
