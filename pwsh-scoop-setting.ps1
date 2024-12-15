cb9702bf266bde7b12eff8da21b4a905459b7cc6
himihiromu <himihiromu@icloud.com>
2024-12-15 17:55:58 +0900

    fix: ダメだったものを消す

--- pwsh-scoop-setting.ps1
+++ pwsh-scoop-setting.ps1
@@ -3,5 +3,4 @@ Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 scoop bucket add extras
 scoop bucket add java
 scoop bucket add games
-scoop bucket add TheRandomLabs https://github.com/TheRandomLabs/Scoop-Bucket.git
-scoop install 7zip adb aomei-partition-assistant-standard appium bun cmake cpu-z crystaldiskinfo dark deepl discord eclipse-java epic-games-launcher filezilla firefox gimp git go googlechrome gradle innounp jetbrains-toolbox latex libreoffice musicbee nmap nvm openjdk openjdk22 openssh openssl pandoc pipx ppsspp putty pwsh python rufus rustup skype slack teamviewer teraterm vivaldi vlc vscode winscp xampp zoom
+scoop install 7zip adb bun cmake cpu-z crystaldiskinfo dark discord eclipse-java epic-games-launcher filezilla firefox gimp git go googlechrome gradle innounp jetbrains-toolbox latex libreoffice nmap nvm openjdk openjdk22 openssh openssl pandoc pipx ppsspp putty pwsh python rufus rustup skype slack teamviewer teraterm vivaldi vlc vscode winscp xampp zoom
