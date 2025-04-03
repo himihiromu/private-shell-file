b7258ffdf4d4af217ba3cc5f30079196ad4774c4
himihiromu <46731750+himihiromu@users.noreply.github.com>
2025-04-03 21:02:03 +0900

    Create pixel-setting.sh

---
+++ pixel-setting.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# check argment
+if [ $# -ne 1 ];then
+    echo "Usage: $0 $ARG1";
+    exit 1
+fi
+
+# install gh
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y))
+sudo mkdir -p -m 755 /etc/apt/keyrings
+out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg
+cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
+sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+# install docker
+# Add Docker's official GPG key:
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian
+ $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# install
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin gh
+
+# nix install
+sh <(curl -L https://nixos.org/nix/install) --daemon
+
+# git setup
+gh auth login --with-token < $0
+
